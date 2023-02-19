ddcca31875ffd6b80794516b805fab7f0a09e362
Hannahvandenbos <hannahvandenbos@gmail.com>
2023-02-19 19:09:31 +0100

    Betere layout + grafiek

--- first_visualisation.R
+++ first_visualisation.R
@@ -11,7 +11,8 @@ twoNE1 <- unique(get_playlist_audio_features("", "37i9dQZF1DZ06evO0Jta80"))
 AKB48 <- unique(get_playlist_audio_features("", "37i9dQZF1DZ06evNZWreOe"))
 
 # 1 = spice girls, 2 = the supremes, 3 = 2NE1, 4 = AKB48
-which_group <- rep(c("Spice Girls (UK)", "The Supremes (US)", "2NE1 (KR)", "AKB48 (JP)"), times=c(49, 43, 50, 36))
+which_group <- rep(c("Spice Girls (UK)", "The Supremes (US)", "2NE1 (KR)", "AKB48 (JP)"), 
+                   times=c(49, 43, 50, 36))
 # combine playlists
 CompletePlaylist <- rbind(SpiceGirls, TheSupremes, twoNE1, AKB48)
 # remove duplicates
@@ -31,11 +32,14 @@ t <- list(
   color = 'black')
 
 coleurs <- c("#0bb4ff", "#9b19f5", "#ffa300", "#e60049")
+hovertext <- paste(CompletePlaylist$track.name, CompletePlaylist$which_group, sep = "<br>")
 bubblechart <- plot_ly(CompletePlaylist, x = ~valence, y = ~energy, color = ~which_group,
-                       colors = coleurs, text = ~track.name, type = 'scatter', mode = 'markers',
-                       plot_bgcolor = "#D6F3D5")
+                       colors = coleurs, text = ~paste("",track.name, "<br>", which_group,
+                                                       "<br> Valence: ", valence, "<br> Energy: ",
+                                                       energy),  hoverinfo = 'text',
+                       type = 'scatter', mode = 'markers')
 
-bubblechart <- bubblechart %>% layout(title = list(text = 'Valence versus energy of all songs',
+bubblechart <- bubblechart %>% layout(title = list(text = 'Valence vs energy as mood indicator',
                                                    font = t, yanchor = 'top'),
                                       xaxis = list(title = list(text = 'Valence', font = 'Consolas'),
                                                    zeroline = FALSE,
@@ -44,5 +48,5 @@ bubblechart <- bubblechart %>% layout(title = list(text = 'Valence versus energy
                                       yaxis = list(title = list(text = 'Energy', font = 'Consolas'),
                                                    zeroline = FALSE,
                                                    range = c(0, 1.1),
-                                                   showgrid = FALSE),
-                                      legend = list(title=list(text=' Girl groups', font = 'Consolas', bgcolor = "#D6F3D5")))
+                                                   showgrid = FALSE), showlegend = FALSE)
+                                      #legend = list(title=list(x = 1, y = 1, text=' Girl groups',font = 'Consolas')))
