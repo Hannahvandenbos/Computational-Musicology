c3182279992b60f113b7c907f97f678114a64ece
Hannahvandenbos <hannahvandenbos@gmail.com>
2023-02-25 20:07:55 +0100

    Hopefully a nice layout

---
+++ PlaylistsPrep.R
@@ -0,0 +1,21 @@
+library(spotifyr)
+library(tidyverse)
+library(dplyr)
+library(plotly)
+
+# Get playlists
+SpiceGirls <- unique(get_playlist_audio_features("", "37i9dQZF1DWWUJdr9ahsbf"))
+SpiceGirls <- SpiceGirls[-1,]
+TheSupremes <- unique(get_playlist_audio_features("", "37i9dQZF1DZ06evO30nCXS"))
+twoNE1 <- unique(get_playlist_audio_features("", "37i9dQZF1DZ06evO0Jta80"))
+AKB48 <- unique(get_playlist_audio_features("", "37i9dQZF1DZ06evNZWreOe"))
+
+# 1 = spice girls, 2 = the supremes, 3 = 2NE1, 4 = AKB48
+which_group <- rep(c("Spice Girls (UK)", "The Supremes (US)", "2NE1 (KR)", "AKB48 (JP)"), 
+                   times=c(49, 43, 50, 36))
+# combine playlists
+CompletePlaylist <- rbind(SpiceGirls, TheSupremes, twoNE1, AKB48)
+# remove duplicates
+CompletePlaylist <- unique(CompletePlaylist)
+# add column with generation
+CompletePlaylist <- cbind(CompletePlaylist, which_group)
--- first_visualisation.R
+++ first_visualisation.R
@@ -1,24 +1,6 @@
-library(spotifyr)
-library(tidyverse)
-library(dplyr)
 library(plotly)
+source('PlaylistsPrep.R')
 
-# Get playlists
-SpiceGirls <- unique(get_playlist_audio_features("", "37i9dQZF1DWWUJdr9ahsbf"))
-SpiceGirls <- SpiceGirls[-1,]
-TheSupremes <- unique(get_playlist_audio_features("", "37i9dQZF1DZ06evO30nCXS"))
-twoNE1 <- unique(get_playlist_audio_features("", "37i9dQZF1DZ06evO0Jta80"))
-AKB48 <- unique(get_playlist_audio_features("", "37i9dQZF1DZ06evNZWreOe"))
-
-# 1 = spice girls, 2 = the supremes, 3 = 2NE1, 4 = AKB48
-which_group <- rep(c("Spice Girls (UK)", "The Supremes (US)", "2NE1 (KR)", "AKB48 (JP)"), 
-                   times=c(49, 43, 50, 36))
-# combine playlists
-CompletePlaylist <- rbind(SpiceGirls, TheSupremes, twoNE1, AKB48)
-# remove duplicates
-CompletePlaylist <- unique(CompletePlaylist)
-# add column with generation
-CompletePlaylist <- cbind(CompletePlaylist, which_group)
 # numerical playlist
 #numeric <- CompletePlaylist %>% 
 #  select(where(is.numeric)) %>%
@@ -28,7 +10,7 @@ CompletePlaylist <- cbind(CompletePlaylist, which_group)
 
 t <- list(
   family = "Consolas",
-  size = 32,
+  size = 13,
   color = 'black')
 
 coleurs <- c("#0bb4ff", "#9b19f5", "#ffa300", "#e60049")
@@ -50,3 +32,122 @@ bubblechart <- bubblechart %>% layout(title = list(text = 'Valence vs energy as 
                                                    range = c(0, 1.1),
                                                    showgrid = FALSE), showlegend = FALSE)
                                       #legend = list(title=list(x = 1, y = 1, text=' Girl groups',font = 'Consolas')))
+
+x_layout <- (xaxis = list(title = list(text = 'Valence', font = 'Consolas'),
+                                zeroline = FALSE,
+                                range = c(0, 1.1),
+                                showgrid = FALSE)) 
+
+y_layout <- (yaxis = list(title = list(text = 'Energy', font = 'Consolas'),
+                                zeroline = FALSE,
+                                range = c(0, 1.1),
+                                showgrid = FALSE))
+
+SG <- CompletePlaylist %>% filter(which_group == "Spice Girls (UK)")
+fig1 <- plot_ly(SG, x = ~valence, y = ~energy, color = 'SG',
+                colors = "#e60049", text = ~paste("",track.name, "<br>", which_group,
+                                                  "<br> Valence: ", valence, "<br> Energy: ",
+                                                  energy),  hoverinfo = 'text',
+                type = 'scatter', mode = 'markers') %>% 
+  layout(xaxis = x_layout, yaxis = y_layout)
+
+TS <- CompletePlaylist %>% filter(which_group == "The Supremes (US)")
+fig2 <- plot_ly(TS, x = ~valence, y = ~energy, color = 'TS',
+                colors = "#0bb4ff", text = ~paste("",track.name, "<br>", which_group,
+                                                  "<br> Valence: ", valence, "<br> Energy: ",
+                                                  energy),  hoverinfo = 'text',
+                type = 'scatter', mode = 'markers') %>% 
+  layout(xaxis = x_layout, yaxis = y_layout)
+
+NE <- CompletePlaylist %>% filter(which_group == "2NE1 (KR)")
+fig3 <- plot_ly(NE, x = ~valence, y = ~energy, color = 'NE',
+                colors = "#ffa300", text = ~paste("",track.name, "<br>", which_group,
+                                                  "<br> Valence: ", valence, "<br> Energy: ",
+                                                  energy),  hoverinfo = 'text',
+                type = 'scatter', mode = 'markers') %>% 
+  layout(xaxis = x_layout, yaxis = y_layout)
+
+AK <- CompletePlaylist %>% filter(which_group == "AKB48 (JP)")
+fig4 <- plot_ly(AK, x = ~valence, y = ~energy, color = 'AK',
+                colors = "#9b19f5", text = ~paste("",track.name, "<br>", which_group,
+                                                  "<br> Valence: ", valence, "<br> Energy: ",
+                                                  energy),  hoverinfo = 'text',
+                type = 'scatter', mode = 'markers') %>% 
+  layout(xaxis = x_layout, yaxis = y_layout)
+
+m <- list(
+  l = 50,
+  r = 25,
+  b = 50,
+  t = 75
+  #pad = 4
+)
+fig <- subplot(fig1, fig2, fig3, fig4, nrows = 2, shareY = TRUE, shareX = TRUE, titleX = FALSE,
+               titleY = FALSE, margin = 0.02)
+fig <- fig %>%layout(title = list(text = "Valence vs energy as mood indicator", font = list(size = 31), yanchor = 'bottom'),
+                     plot_bgcolor="#f4ebfe", margin = m, showlegend = FALSE)
+
+# Update title
+annotations = list(
+  list( 
+    x = 0.23,  
+    y = 1.0,  
+    text = "Spice Girls (UK)",  
+    xref = "paper",
+    yref = "paper",
+    xanchor = "center",  
+    yanchor = "bottom",  
+    showarrow = FALSE 
+  ),  
+  list( 
+    x = 0.78,  
+    y = 1,  
+    text = "The Supremes (US)",  
+    xref = "paper",  
+    yref = "paper",  
+    xanchor = "center",  
+    yanchor = "bottom",  
+    showarrow = FALSE 
+  ),  
+  list( 
+    x = 0.23,  
+    y = 0.48,  
+    text = "2NE1 (KR)",  
+    xref = "paper",  
+    yref = "paper",  
+    xanchor = "center",  
+    yanchor = "bottom",  
+    showarrow = FALSE 
+  ),
+  list( 
+    x = 0.78,  
+    y = 0.48,  
+    text = "AKB48 (JP)",  
+    xref = "paper",  
+    yref = "paper",  
+    xanchor = "center",  
+    yanchor = "bottom",  
+    showarrow = FALSE 
+  ),
+  list(
+    x = -0.06, 
+    y = 0.5, 
+    text = "Energy",
+    font = list(size = 18),
+    textangle = 270,
+    showarrow = F, 
+    xref='paper', 
+    yref='paper', 
+    size=48),
+  list(
+    x = 0.5, 
+    y = -0.09, 
+    text = "Valence",
+    font = list(size = 18),
+    showarrow = F, 
+    xref='paper', 
+    yref='paper', 
+    size=48))
+
+fig <- fig %>%layout(annotations = annotations) 
+fig
